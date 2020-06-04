611d5030a36ef2e19510a3cc2c1a90a426684c21
Marc Davies <marcdaviesriot@gmail.com>
2020-06-03 23:04:04 -0700

    create POST  method

--- js/app.js
+++ js/app.js
@@ -1,10 +1,37 @@
 const http = new easyHTTP();
 
-// Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
+// // Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// // Get Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, post) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Create data
+const data = {
+  title: 'New Post',
+  body: 'This is a new post',
+};
+
+// Create post
+http.post('https://jsonplaceholder.typicode.com/posts', data, function (
+  err,
+  post
+) {
   if (err) {
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(post);
   }
 });
