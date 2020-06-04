28907e201b75291f9acdefb3502d9b761a5680b7
Marc Davies <marcdaviesriot@gmail.com>
2020-06-03 23:06:05 -0700

    create PUT  method

--- js/app.js
+++ js/app.js
@@ -25,7 +25,19 @@ const data = {
 };
 
 // Create post
-http.post('https://jsonplaceholder.typicode.com/posts', data, function (
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Update post
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (
   err,
   post
 ) {
