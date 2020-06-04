76486718c76eb2e49463817acd7c6a6017e7b878
Marc Davies <marcdaviesriot@gmail.com>
2020-06-04 00:48:42 -0700

    add get and post methods

---
+++ js/easyhttp2.js
@@ -0,0 +1,36 @@
+/**
+EasyHTTP Library
+Library for making HTTP requests
+
+@version 2.0.0
+@author Marc Davies
+@licence MIT
+**/
+
+class EasyHTTP {
+  // Make an HTTP Get Request
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
+
+  // Make an HTTP Post Request
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
+    });
+  }
+}
